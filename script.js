74ed8d30979ab3762a83a77cf4ca1addd735cda8
damian <bd.rojasvidela@gmail.com>
2025-09-21 16:13:53 -0300

    Agrega funcionalidad para crear hábitos

--- script.js
+++ script.js
@@ -5,42 +5,64 @@ let fechaActual = document.getElementById("fechaActual");
 let mesActual = document.getElementById("mesActual");
 let diasMes = new Date(fecha.getFullYear(), fecha.getMonth() + 1, 0).getDate();
 
-let racha = [];
+let contenedorHabitos = document.querySelector(".contenedor-habitos")
 
 let agregarHabito = document.getElementById("agregarHabito");
 
 fechaActual.innerHTML = fecha.getDate() + "/" + (fecha.getMonth() + 1) + "/" + fecha.getFullYear();
 mesActual.innerHTML = meses[fecha.getMonth()];  
 
-// Crea los botones según los días que tiene el mes
-for(let dia = 1; dia <= diasMes; dia++) {
-    let button = document.createElement("button");
-    button.type = "button";
-    button.id = "dia-" + dia;
-    button.innerHTML = dia;
-    document.querySelector(".dias").appendChild(button);
-}
+agregarHabito.addEventListener("click", function() {
+    let nombreHabito = prompt("Ingrese el nombre del hábito que desea agregar");
+    let divHabito = document.createElement("div")
+    divHabito.classList.add("habito")
 
-//Selecciona los dias y se los asigna a la variable button
-for (let dia = 1; dia <= diasMes; dia++) {
-	let button = document.getElementById("dia-" + dia);
-    //Habilita los botones hsta el dia actual
-	if (dia <= fecha.getDate()) {
-		button.addEventListener("click", function () {
-			button.classList.toggle("racha");
-            //Agrega el día marcado a la lista racha
-            if (button.classList.contains("racha") && !racha.includes(dia)) {
-                racha.push(dia);
-                console.log(racha);
-            }
-            //"Elimina" el día que fue desmarcado de la lista racha
-            else if (!button.classList.contains("racha") && racha.includes(dia)) {
-                racha = racha.filter(d => d !== dia)
-                console.log(racha);
-            }
-		});
+    let divNombreHabito = document.createElement("div")
+    divNombreHabito.classList.add("nombre-habito")
+    divNombreHabito.innerHTML = nombreHabito
+
+    let divDias = document.createElement("div")
+    divDias.classList.add("dias")
+
+    contenedorHabitos.appendChild(divHabito)
+    divHabito.appendChild(divNombreHabito)
+    divHabito.appendChild(divDias)
+
+    //Crea un array vacio para cada hábito agregado
+    let racha = [];
+
+    crearDias(nombreHabito, racha, divDias)
+})
+
+// Crea los botones según los días que tiene el mes
+function crearDias(nombreHabito, racha, divDias) {
+	for (let dia = 1; dia <= diasMes; dia++) {
+		let button = document.createElement("button");
+		button.type = "button";
+		//button.id = nombreHabito + "-dia-" + dia;
+		button.innerHTML = dia;
+
+        //Habilita los botones hasta el dia actual
+		if (dia <= fecha.getDate()) {
+			button.addEventListener("click", function () {
+				button.classList.toggle("racha");
+				//Agrega el día marcado a la lista racha
+				if (button.classList.contains("racha") && !racha.includes(dia)) {
+					racha.push(dia);
+					console.log(nombreHabito, racha);
+				}
+				//"Elimina" el día que fue desmarcado de la lista racha
+				else if (!button.classList.contains("racha") && racha.includes(dia)) {
+					racha = racha.filter((d) => d !== dia);
+					console.log(nombreHabito, racha);
+				}
+			});
+		} 
         //Desactiva los botones de los días que son mayores al día actual
-	} else {
-		button.disabled = true;
+        else {
+			button.disabled = true;
+		}
+
+		divDias.appendChild(button);
 	}
 }

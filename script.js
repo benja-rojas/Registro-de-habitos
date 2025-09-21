1ac513bf86735f26b9a5b7d63302af4670ec36bb
damian <bd.rojasvidela@gmail.com>
2025-09-21 14:00:59 -0300

    Agrega funcionalidad para seleccionar días y marcar rachas en el calendario

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let fechaActual = document.getElementById("fechaActual");
 let mesActual = document.getElementById("mesActual");
 let diasMes = new Date(fecha.getFullYear(), fecha.getMonth() + 1, 0).getDate();
 
+let racha = [];
+
 let agregarHabito = document.getElementById("agregarHabito");
 
 fechaActual.innerHTML = fecha.getDate() + "/" + (fecha.getMonth() + 1) + "/" + fecha.getFullYear();
@@ -17,4 +19,28 @@ for(let dia = 1; dia <= diasMes; dia++) {
     button.id = "dia-" + dia;
     button.innerHTML = dia;
     document.querySelector(".dias").appendChild(button);
+}
+
+//Selecciona los dias y se los asigna a la variable button
+for (let dia = 1; dia <= diasMes; dia++) {
+	let button = document.getElementById("dia-" + dia);
+    //Habilita los botones hsta el dia actual
+	if (dia <= fecha.getDate()) {
+		button.addEventListener("click", function () {
+			button.classList.toggle("racha");
+            //Agrega el día marcado a la lista racha
+            if (button.classList.contains("racha") && !racha.includes(dia)) {
+                racha.push(dia);
+                console.log(racha);
+            }
+            //"Elimina" el día que fue desmarcado de la lista racha
+            else if (!button.classList.contains("racha") && racha.includes(dia)) {
+                racha = racha.filter(d => d !== dia)
+                console.log(racha);
+            }
+		});
+        //Desactiva los botones de los días que son mayores al día actual
+	} else {
+		button.disabled = true;
+	}
 }

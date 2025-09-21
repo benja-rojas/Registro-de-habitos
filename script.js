f5ab85506cad001d11cd934124a8a8123afe987d
damian <bd.rojasvidela@gmail.com>
2025-09-21 17:15:04 -0300

    Agrega funcionalidad para guardar la racha en el nuevo hábito creado

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let fechaActual = document.getElementById("fechaActual");
 let mesActual = document.getElementById("mesActual");
 let diasMes = new Date(fecha.getFullYear(), fecha.getMonth() + 1, 0).getDate();
 
+let habitos = [];
+
 let contenedorHabitos = document.querySelector(".contenedor-habitos")
 
 let agregarHabito = document.getElementById("agregarHabito");
@@ -12,30 +14,41 @@ let agregarHabito = document.getElementById("agregarHabito");
 fechaActual.innerHTML = fecha.getDate() + "/" + (fecha.getMonth() + 1) + "/" + fecha.getFullYear();
 mesActual.innerHTML = meses[fecha.getMonth()];  
 
-agregarHabito.addEventListener("click", function() {
-    let nombreHabito = prompt("Ingrese el nombre del hábito que desea agregar");
-    let divHabito = document.createElement("div")
-    divHabito.classList.add("habito")
+agregarHabito.addEventListener("click", function () {
+	let nombreHabito = prompt("Ingrese el nombre del hábito que desea agregar");
+	if (nombreHabito != "" && nombreHabito != null) {
+		let divHabito = document.createElement("div");
+		divHabito.classList.add("habito");
 
-    let divNombreHabito = document.createElement("div")
-    divNombreHabito.classList.add("nombre-habito")
-    divNombreHabito.innerHTML = nombreHabito
+		let divNombreHabito = document.createElement("div");
+		divNombreHabito.classList.add("nombre-habito");
+		divNombreHabito.innerHTML = nombreHabito;
 
-    let divDias = document.createElement("div")
-    divDias.classList.add("dias")
+        //Crea un nuevo objeto hábito y lo agrega al array de hábitos
+        let nuevoHabito = {
+            nombre: nombreHabito,
+            racha: []
+        }
 
-    contenedorHabitos.appendChild(divHabito)
-    divHabito.appendChild(divNombreHabito)
-    divHabito.appendChild(divDias)
+        habitos.push(nuevoHabito);
+        console.log(habitos)
 
-    //Crea un array vacio para cada hábito agregado
-    let racha = [];
+		let divDias = document.createElement("div");
+		divDias.classList.add("dias");
 
-    crearDias(nombreHabito, racha, divDias)
-})
+		contenedorHabitos.appendChild(divHabito);
+		divHabito.appendChild(divNombreHabito);
+		divHabito.appendChild(divDias);
+
+
+		crearDias(nuevoHabito, divDias);
+	}else if (nombreHabito === ""){
+        alert("El nombre del hábito no puede estar vacío")
+    }
+});
 
 // Crea los botones según los días que tiene el mes
-function crearDias(nombreHabito, racha, divDias) {
+function crearDias(nuevoHabito, divDias) {
 	for (let dia = 1; dia <= diasMes; dia++) {
 		let button = document.createElement("button");
 		button.type = "button";
@@ -47,14 +60,14 @@ function crearDias(nombreHabito, racha, divDias) {
 			button.addEventListener("click", function () {
 				button.classList.toggle("racha");
 				//Agrega el día marcado a la lista racha
-				if (button.classList.contains("racha") && !racha.includes(dia)) {
-					racha.push(dia);
-					console.log(nombreHabito, racha);
+				if (button.classList.contains("racha") && !nuevoHabito.racha.includes(dia)) {
+					nuevoHabito.racha.push(dia);
+					console.log(nuevoHabito.nombre, nuevoHabito.racha);
 				}
 				//"Elimina" el día que fue desmarcado de la lista racha
-				else if (!button.classList.contains("racha") && racha.includes(dia)) {
-					racha = racha.filter((d) => d !== dia);
-					console.log(nombreHabito, racha);
+				else if (!button.classList.contains("racha") && nuevoHabito.racha.includes(dia)) {
+					nuevoHabito.racha = nuevoHabito.racha.filter((d) => d !== dia);
+					console.log(nuevoHabito.nombre, nuevoHabito.racha);
 				}
 			});
 		} 
